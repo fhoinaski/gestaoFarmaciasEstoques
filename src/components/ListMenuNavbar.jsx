4af7544cb7133c0c65f27479665d200cd609f3fe
fhoinaski <sukinodoncai@gmail.com>
2023-04-18 00:03:43 -0300

    componente que renderiza a lista no navbar

--- src/components/ListMenuNavbar.jsx
+++ src/components/ListMenuNavbar.jsx
@@ -1,13 +1,14 @@
+import { NavDropdown } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 export const ListMenuNavbar = ({ nomeMenu, listaItens }) => {
-    return (
-      <div className="hidden md:block group relative bg-white py-2 px-3 rounded-lg">
-        <span className="text-orange-400 font-bold cursor-pointer">{nomeMenu}</span>
-        <ul className="absolute left-0 cursor-pointer hidden w-48 group-hover:block bg-white rounded-md shadow-lg py-2">
-          {listaItens.map((item) => (
-            <li className="hover:bg-gray-100 px-4 py-2">{item}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  };
-  
+  return (
+    <NavDropdown title={nomeMenu} className="text-orange-400 font-bold">
+      {listaItens.map((item) => (
+        <LinkContainer to={item.caminho} key={item.nome}>
+          <NavDropdown.Item>{item.nome}</NavDropdown.Item>
+        </LinkContainer>
+      ))}
+    </NavDropdown>
+  );
+};
