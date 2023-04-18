b7f48e86e4b8b741f96e19a5117caa0e0908246f
fhoinaski <sukinodoncai@gmail.com>
2023-04-18 00:01:34 -0300

    update de rotas

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,23 +1,54 @@
-
-import { createBrowserRouter } from 'react-router-dom';
-import App  from './App';
+import { Outlet, createBrowserRouter } from 'react-router-dom';
+import App from './App';
 import { Home } from "./pages/Home";
 import Login from "./components/usuario/Login"
 import CadastroFarmacia from "./pages/CadastroFarmacia"
+import { FarmaciaProvider } from './contexts/farmaciaContext/FarmaciaProvider';
+import { MedicamentoProvider } from './contexts/medicamentoContext/MedicamentoProvider';
+import CadastroMedicamento from './pages/CadastroMedicamento';
+import Medicamentos from './pages/Medicamentos';
+import Farmacias from './pages/Farmacias';
 
-//exercicio 1 adicionar as rodas home e Quemsou
 
-export const appRouter = createBrowserRouter([
+const FarmaciaOutlet = () => {
+  return (
+    <FarmaciaProvider>
+      <Outlet />
+    </FarmaciaProvider>
+  );
+};
+
+const MedicamentoOutlet = () => {
+  return (
+    <MedicamentoProvider>
+      <Outlet />
+    </MedicamentoProvider>
+  );
+};
+
+export const AppRouter = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
       { path: '/', element: <Home /> },
       { path: '/login', element: <Login /> },
-      { path: '/cadastro-farmacia', element: <CadastroFarmacia /> },
-      
-       
+      {
+        path: '/farmacias',
+        element: <FarmaciaOutlet />,
+        children: [
+          { path: '', element: <Farmacias /> },
+          { path: 'cadastrar', element: <CadastroFarmacia /> },
+        ],
+      },
+      {
+        path: '/medicamentos',
+        element: <MedicamentoOutlet />,
+        children: [
+          { path: '', element: <Medicamentos /> },
+          { path: 'cadastrar', element: <CadastroMedicamento /> },
+        ],
+      },
     ],
   },
 ]);
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { appRouter } from "./Router";
+import { AppRouter } from './Router';
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={appRouter} />
+  <RouterProvider router={AppRouter} />
 );
