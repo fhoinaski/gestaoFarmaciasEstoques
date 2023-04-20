cd220ec4d1dda1f2bd9b2a0e8ac339898233f2cc
fhoinaski <sukinodoncai@gmail.com>
2023-04-20 02:36:30 -0300

    atualização no estilo e criação de novo layout

--- src/components/CardProdutos.jsx
+++ src/components/CardProdutos.jsx
@@ -1,23 +1,51 @@
-const CardProdutos = ({title,description}) => {
+import { useState } from "react";
+import { MdExpandMore } from 'react-icons/md';
+
+
+const CardProdutos = ({ title, description, onClick }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  // gira o card
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-full" src="https://source.unsplash.com/random" alt="product" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">
-          {description}
-        </p>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #product
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #category
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-          #brand
-        </span>
+    <div className="w-2/3 sm:w-10/12 items-center justify-center flex flex-wrap max-w-sm rounded overflow-hidden text-center shadow-lg my-1">
+      <div className="flex justify-center mt-2 w-full"><img className="w-20" src="/remedio.png" alt="product" /></div>
+      <div
+        className={`relative w-72  bg-white rounded-xl shadow-lg transition-all duration-700 ease-in-out`}
+        style={{
+          transformStyle: 'preserve-3d',
+          perspective: '1000px',
+          transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)',
+        }}
+        onClick={handleClick}
+      >
+        <div className="px-0 mx-auto py-4 w-full  rounded-xl bg-gray-50 ">
+          <div className="font-bold text-sm mb-2">{title}</div>
+          <p className="text-gray-700 text-base">
+            {description}
+          </p>
+          <div
+            className="justify-end  flex items-center mx-auto h-5"
+            onClick={onClick}
+          >
+            <span className="flex items-center cursor-pointer bg-gray-100 px-2 rounded-xl -mb-4  hover:bg-slate-200 " >Ver detalhes <MdExpandMore className="ml-2" /></span>
+          </div>
+
+        </div>
+        <div
+          className={`absolute top-0 left-0 w-full h-full bg-gray-50 rounded-xl z-10  ${isFlipped ? 'opacity-100 ' : 'opacity-0 '} transition-opacity duration-1000`}
+          style={{ backfaceVisibility: 'hidden', transform: 'rotateY(180deg)' }}
+        >
+          <div className="p-0">
+            <h2 className="text-xl font-bold mb-2">Back Side</h2>
+            <p className="text-orange-700">
+              Nunc laoreet nulla id magna pellentesque, eu pulvinar nisi iaculis. Vivamus dapibus
+
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
