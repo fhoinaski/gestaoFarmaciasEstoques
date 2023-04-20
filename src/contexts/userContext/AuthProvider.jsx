67451a6f6fe9baf92ca88809e327f43f17052f30
fhoinaski <sukinodoncai@gmail.com>
2023-04-20 02:47:31 -0300

    mudanças para gerenciar o login no local storage salvando o usuario como true enquanto logado

--- src/contexts/userContext/AuthProvider.jsx
+++ src/contexts/userContext/AuthProvider.jsx
@@ -1,29 +1,39 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext();
 
 export { AuthContext };
 
+
+
 export const AuthProvider = ({ children }) => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
     const navigate = useNavigate();
     const [userData, setUserData] = useState({
         email: '',
         password: '',
     });
 
-    // Função para logar o usuário.
+    // Define o estado inicial de isAuthenticated com base no valor armazenado no localStorage, caso contrário, será falso.
+    const initialAuthState = JSON.parse(localStorage.getItem('isAuthenticated')) || false;
+    const [isAuthenticated, setIsAuthenticated] = useState(initialAuthState);
+
+    // Atualiza o valor de 'isAuthenticated' no localStorage sempre que ele mudar.
+    useEffect(() => {
+        localStorage.setItem('isAuthenticated', JSON.stringify(isAuthenticated));
+    }, [isAuthenticated]);
+
+    // Função para autenticar o usuário.
     const loginUser = async ({ email, password }) => {
         const user = {
             email,
             password,
         };
 
-        if (validaPassword(password)) {
+        // Verifica se a senha é válida e, em caso afirmativo, autentica o usuário, armazena os dados do usuário e redireciona para a página desejada.
+        if (validatePassword(password)) {
             setIsAuthenticated(true);
             localStorage.setItem("user", JSON.stringify(user));
-            //redireciona o usuario após login com sucesso
             navigate('/farmacias/cadastrar');
             return user;
         } else {
@@ -32,35 +42,32 @@ export const AuthProvider = ({ children }) => {
         }
     };
 
-    // função para deslogar o usuário
+    // Função para deslogar o usuário, removendo os dados do usuário do localStorage e redirecionando para a página inicial.
     const logoutUser = () => {
         localStorage.removeItem("user");
         setIsAuthenticated(false);
         navigate('/');
     };
 
-    // validar senha
-    const validaPassword = (password) => {
+    // Função para validar a senha com base em uma expressão regular.
+    const validatePassword = (password) => {
         const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
         return regex.test(password);
     };
 
-    // atualizar dados do usuário
+    // Função para atualizar os dados do usuário no estado userData.
     const updateUserData = (name, value) => {
         setUserData({ ...userData, [name]: value });
     };
-
-    
 
     const value = {
         isAuthenticated,
         userData,
         loginUser,
         logoutUser,
-        validaPassword,
+        validatePassword,
         updateUserData,
-        
-    };    
+    };
 
     return (
         <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
