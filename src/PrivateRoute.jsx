3bdc5972c138096d6f6c36f8a26da4da3ce1d18d
fhoinaski <sukinodoncai@gmail.com>
2023-04-20 02:34:26 -0300

    criado arquivo que gerencia as rotas privadas

---
+++ src/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+// src/PrivateRoute.jsx
+import { Navigate, useLocation } from "react-router-dom";
+import {useAuthState}  from "./contexts/userContext/useAuth";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { isAuthenticated, loading } = useAuthState ();
+  const location = useLocation();
+
+  if (loading) {
+    return <div>Carregando...</div>;    
+  }
+
+  return isAuthenticated ? (
+    <>{children}</>
+  ) : (
+    <Navigate to="/login" replace state={{ from: location }} />
+  );
+};
+
+export default PrivateRoute;
