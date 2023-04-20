07d431d73c7cc6062ea3b49d49ea5f34a8602e56
fhoinaski <sukinodoncai@gmail.com>
2023-04-20 18:34:28 -0300

    correção do filtro de busca do campo valor

--- src/components/SelectOption.jsx
+++ src/components/SelectOption.jsx
@@ -4,9 +4,9 @@ const SelectOption = ({ value, onChange }) => {
   return (
     <div className="select-option">
       <select value={value} onChange={onChange} className="select-input">
-        <option value="todos">Todos Medicamentos</option>
-        <option value="comum">Medicamento Comum</option>
-        <option value="controlado">Medicamento Controlado</option>
+        <option value="Todos">Todos Medicamentos</option>
+        <option value="Comum">Medicamento Comum</option>
+        <option value="Controlado">Medicamento Controlado</option>
       </select>
     </div>
   );
