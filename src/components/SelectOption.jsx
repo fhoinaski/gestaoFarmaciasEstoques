3f530b9c323f1ec488697520ede907f667c4cba5
fhoinaski <sukinodoncai@gmail.com>
2023-04-21 11:52:38 -0300

    atualizado o estilo visual e

--- src/components/SelectOption.jsx
+++ src/components/SelectOption.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const SelectOption = ({ value, onChange }) => {
   return (
-    <div className="select-option">
-      <select value={value} onChange={onChange} className="select-input">
-        <option value="todos">Todos Medicamentos</option>
-        <option value="Comum">Medicamento Comum</option>
-        <option value="Controlado">Medicamento Controlado</option>
+    <div className="select-option  rounded-xl">
+      <select value={value} onChange={onChange} className="select-input h-6 rounded-md border-2 border-gray-300 w-32 md:w-auto focus:outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent">
+        <option className='font-medium text-orange-500' value="todos">Todos Medicamentos</option>
+        <option className='font-medium text-orange-500'value="Comum">Medicamento Comum</option>
+        <option className='font-medium text-orange-500'value="Controlado">Medicamento Controlado</option>
       </select>
     </div>
   );
