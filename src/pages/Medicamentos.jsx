78fb32a20faca99b2f05f95671bff4d9648bf783
fhoinaski <sukinodoncai@gmail.com>
2023-04-21 11:53:32 -0300

    substituido o card criado o input de busca e correção no estilo visual

--- src/pages/Medicamentos.jsx
+++ src/pages/Medicamentos.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import CardProdutos from '../components/CardProdutos';
 import Carregando from '../components/Carregando';
 import { useMedicamentoState } from '../contexts/medicamentoContext/useMedicamento';
+import {AiOutlineSearch} from "react-icons/ai"
 
 import SelectOption from '../components/SelectOption';
 
@@ -53,24 +54,28 @@ const Medicamentos = () => {
         </div>
       ) : (
         <div>
-          <SelectOption value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)} />
+          <div className='flex justify-center items-center h-10 bg-orange-50'>
+            
+            <div className="flex mr-4 items-center justify-center">
+            <input className=' h-6 rounded-md w-28 md:w-auto border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-orange-600 focus:border-transparent text-orange-400 text-center' type='text' placeholder='Pesquisar Medicamento' onChange={(e) => setValorBuscado(e.target.value)} />
+            <AiOutlineSearch className="text-orange-500 w-5 h-6" />
+            </div>
+
+            <div className='flex items-center justify-center m-0'>
+              <SelectOption value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)} />
+            </div>
+          </div>
           <div className="flex justify-center w-full py-4">
             <div className="mb-4">
           
             </div>
-            <InfoModal
-              isOpen={isOpen}
-              onClose={() => setIsOpen(false)}
-              objetoInfo={detalheMedicamento}
-              titulo={detalheMedicamento?.nomeMedicamento}
-            />
+
             <div className="m-0 flex flex-wrap w-full left-0 right-0 justify-center gap-4">
               {(medicamentosFiltrados.length > 0 ? medicamentosFiltrados : todosMedicamentos).map((medicamentos, index) => (
                 <div className="w-full md:w-1/3 lg:w-1/4 xl:w-1/4 px-1 flex justify-center"
                   key={medicamentos.id}>
                   <CardProdutos
-                    title={medicamentos.nomeMedicamento}
-                    description={medicamentos.nomeLaboratorio}
+                    produtoInfo={medicamentos}
                     onClick={() => mostrarMedicamento(medicamentos.id)}
                   />
                 </div>
