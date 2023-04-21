fa3497f17227c925f5b8564dc5e60d58caaa6bab
fhoinaski <sukinodoncai@gmail.com>
2023-04-21 14:14:28 -0300

    pequenas alterações no texto de alerta e no estilo

--- src/components/usuario/Login.jsx
+++ src/components/usuario/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
         const user = await loginUser(userData);
         console.log(user);
         if (user === null) {
-          setErro('Senha fora dos requisitos');
+          setErro('Senha deve ter Letras e Números');
         } else {
           setErro('');             
         }
@@ -21,7 +21,7 @@ const Login = () => {
     
 
     return (
-        <Container className="h-100 my-6">
+        <Container className="h-96 my-10">
         <Row className="h-100 justify-content-center align-items-center">
             <Col xs={12} md={6} lg={4}>
                 <Card>
@@ -53,9 +53,9 @@ const Login = () => {
                                 />
                             </Form.Group>
                             {erro && (
-                                <Alert variant="danger" className="mb-3">{erro}</Alert>
+                                <Alert variant="danger" className="mb-3 text-center">{erro}</Alert>
                             )}
-                            <Button variant="warning" type="submit" className="w-100 text-uppercase">
+                            <Button  type="submit" className="w-100 text-uppercase bg-orange-500 border-none hover:bg-orange-600">
                                 Entrar
                             </Button>
                         </Form>
